010118cc32b8258f99622724961e95f0f97071ea
Tristan <tristan.leblouch@gmail.com>
2025-12-05 05:01:35 +0100

    test

--- src/components/blocks/desktop.tsx
+++ src/components/blocks/desktop.tsx
@@ -5,6 +5,7 @@ import Mail from '../layouts/mail'
 import Troll from '../layouts/troll'
 import Snake from '../layouts/snake'
 import Window from './window'
+import Files from './files'
 
 
 function Desktop() {
@@ -85,6 +86,7 @@ function Desktop() {
             <AppIcon onOpenApp={handleOpenApp} appName="Mail" />
             <AppIcon onOpenApp={handleOpenApp} appName="Troll" />
             <AppIcon onOpenApp={handleOpenApp} appName="Snake" />
+            <AppIcon onOpenApp={handleOpenApp} appName="Files" />
 
       {openApps.map((app) => (
         app[1] === "MainGame" 
@@ -95,6 +97,8 @@ function Desktop() {
         ? <Mail key={app[0]} uuid={app[0]} title={app[1]} initialPosition={{ x: app[2], y: app[3] }} initialSize={{ width: 300, height: 200 }} onClose={() => handleCloseWindow(app[0])} onClick={(position) => handleWindowClick(app[0], position)} shouldBlink={blinkingWindows.has(app[0])} /> 
         : app[1] === "Snake" 
         ? <Snake key={app[0]} uuid={app[0]} title={app[1]} initialPosition={{ x: app[2], y: app[3] }} initialSize={{ width: 300, height: 200 }} onClose={() => handleCloseWindow(app[0])} onClick={(position) => handleWindowClick(app[0], position)} shouldBlink={blinkingWindows.has(app[0])} /> 
+        : app[1] === "Files"
+        ? <Files key={app[0]} uuid={app[0]} title={app[1]} initialPosition={{ x: app[2], y: app[3] }} initialSize={{ width: 300, height: 200 }} onClose={() => handleCloseWindow(app[0])} onClick={(position) => handleWindowClick(app[0], position)} shouldBlink={blinkingWindows.has(app[0])} />
         : <Window key={app[0]} uuid={app[0]} title={app[1]} initialPosition={{ x: app[2], y: app[3] }} initialSize={{ width: 300, height: 200 }} onClose={() => handleCloseWindow(app[0])} onClick={(position) => handleWindowClick(app[0], position)} shouldBlink={blinkingWindows.has(app[0])}>
           <div className="w-full h-full"></div>
         </Window>
